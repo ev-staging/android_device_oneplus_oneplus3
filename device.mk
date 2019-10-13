e899c0dc335ae27bad38b9c2b2ead978ca26cdd4
Nick Reuter <nreuter85@gmail.com>
2019-10-12 23:58:01 -0500

    Revert "Revert "op3: Switch to NQ NFC stack""
    This reverts commit 0b64d7f4c02a48d724c9f6d8c3c245b1ac55c02f.

--- device.mk
+++ device.mk
@@ -364,19 +364,26 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
+    NQNfcNci \
+    libnqnfc-nci \
+    libnqnfc_nci_jni \
+    nfc_nci.nqx.default \
+    libp61-jcop-kit \
+    com.nxp.nfc.nq \
+    com.nxp.nfc.nq.xml \
+    nqnfcee_access.xml \
+    nqnfcse_access.xml \
     Tag \
-    android.hardware.nfc@1.1-service
+    com.android.nfc_extras \
+    vendor.nxp.hardware.nfc@1.1-service \
+    nfc_nci.nqx.default.hw
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
-# OEM Unlock reporting
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.oem_unlock_supported=1
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.nfc_nci=nqx.default
 
 # OMX
 PRODUCT_PACKAGES += \
